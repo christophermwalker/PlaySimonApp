4dfa1243df9dfe86f869a55eae99e3cab5bc3e81
WalkerC <christophermwalker@gmail.com>
2018-03-06 11:33:57 -0500

    Simon Activity Images Added

--- MainActivity.java
+++ MainActivity.java
@@ -1,13 +1,30 @@
 package edu.apsu.cwalker61.playsimonapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        int ids[]={R.id.play_button};
+        for (int id : ids) {
+            Button btn=(Button) findViewById(id);
+            btn.setOnClickListener(this);
+        }
+    }
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.play_button) {
+            Intent intent=new Intent(
+                    getApplicationContext(),
+                    SimonActivity.class);
+            startActivity(intent);
+        }
     }
 }
---
+++ SimonActivity.java
@@ -0,0 +1,20 @@
+package edu.apsu.cwalker61.playsimonapp;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class SimonActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_simon);
+
+
+    }
+
+}
