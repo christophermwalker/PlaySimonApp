66f798e76642967acfc499fe1d9c18066732e449
Coel625 <btaylor40@my.apsu.edu>
2018-03-06 20:03:21 -0600

    Set up general activity format as well as some basic colors.

---
+++ app/src/main/java/edu/apsu/csci/playsimonapp/StartActivity.java
@@ -0,0 +1,45 @@
+package edu.apsu.csci.playsimonapp;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class StartActivity extends Activity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+
+        int ids[]={R.id.play_button, R.id.highscore_button, R.id.about_button};
+        for (int id : ids) {
+            Button b =(Button) findViewById(id);
+            b.setOnClickListener(this);
+        }
+    }
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.play_button) {
+            Intent intent = new Intent(
+                    getApplicationContext(),
+                    GameActivity.class);
+            startActivity(intent);
+        }
+
+        if (view.getId() == R.id.highscore_button) {
+            Intent intent = new Intent(
+                    getApplicationContext(),
+                    HighscoreActivity.class);
+            startActivity(intent);
+        }
+
+        if (view.getId() == R.id.about_button) {
+            Intent intent = new Intent(
+                    getApplicationContext(),
+                    AboutActivity.class);
+            startActivity(intent);
+        }
+    }
+}
