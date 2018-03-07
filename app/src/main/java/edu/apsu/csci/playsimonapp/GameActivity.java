af195084134ae71653723fe1e801a3ecfb37959a
Coel625 <btaylor40@my.apsu.edu>
2018-03-06 23:49:14 -0600

    Added some buttons with some temporary code and colors. Also added app icon.

--- app/src/main/java/edu/apsu/csci/playsimonapp/GameActivity.java
+++ app/src/main/java/edu/apsu/csci/playsimonapp/GameActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class GameActivity extends Activity implements View.OnClickListener {
 
@@ -15,33 +16,54 @@ public class GameActivity extends Activity implements View.OnClickListener {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
 
-        int ids[]={R.id.play_button, R.id.highscore_button, R.id.about_button};
+        ImageButton ib = (ImageButton) findViewById(R.id.green_button);
+        ib.setOnClickListener(this);
+
+        ImageButton ib2 = (ImageButton) findViewById(R.id.red_button);
+        ib2.setOnClickListener(this);
+
+        ImageButton ib3 = (ImageButton) findViewById(R.id.yellow_button);
+        ib3.setOnClickListener(this);
+
+        ImageButton ib4 = (ImageButton) findViewById(R.id.blue_button);
+        ib4.setOnClickListener(this);
+
+       /* int ids[]={R.id.green_button, R.id.red_button, R.id.yellow_button, R.id.blue_button};
         for (int id : ids) {
-            Button b =(Button) findViewById(id);
-            b.setOnClickListener(this);
-        }
+            ImageButton ib =(ImageButton) findViewById(id);
+            ib.setOnClickListener(this);
+        } */
     }
     @Override
     public void onClick(View view) {
-        if (view.getId() == R.id.play_button) {
-            String etStaticString = "0";
-            Intent intent = new Intent(getApplicationContext(), HighscoreActivity.class);
-            intent.putExtra(SCORE_KEY, etStaticString);
-            startActivity(intent);
+        Integer[] images = {
+                R.drawable.real_green_button, R.drawable.real_red_button,
+                R.drawable.real_yellow_button, R.drawable.real_blue_button,
+                R.drawable.real_bright_green_button, R.drawable.real_bright_red_button,
+                R.drawable.real_bright_yellow_button, R.drawable.real_bright_blue_button
+        };
+
+        ImageButton ib = (ImageButton) findViewById(R.id.green_button);
+        ImageButton ib2 = (ImageButton) findViewById(R.id.red_button);
+        ImageButton ib3 = (ImageButton) findViewById(R.id.yellow_button);
+        ImageButton ib4 = (ImageButton) findViewById(R.id.blue_button);
+
+        int count = 0;
+
+        if (view.getId() == R.id.green_button) {
+            count = 1;
         }
 
-        if (view.getId() == R.id.highscore_button) {
-            String etStaticString = "85";
-            Intent intent = new Intent(getApplicationContext(), HighscoreActivity.class);
-            intent.putExtra(SCORE_KEY, etStaticString);
-            startActivity(intent);
+        if (view.getId() == R.id.red_button) {
+            count = 2;
         }
 
-        if (view.getId() == R.id.about_button) {
-            String etStaticString = "102";
-            Intent intent = new Intent(getApplicationContext(), HighscoreActivity.class);
-            intent.putExtra(SCORE_KEY, etStaticString);
-            startActivity(intent);
+        if (view.getId() == R.id.yellow_button) {
+            count = 3;
+        }
+
+        if (view.getId() == R.id.blue_button) {
+            count = 4;
         }
     }
 }
